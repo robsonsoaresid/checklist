9964c758284d5b63ca9f44b4a62a23dd77f80c31
robsonsoaresid <robemsuave@gmail.com>
2026-08-03 14:35:37 -0300

    Reagindo a cliques

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 import './dialog.style.css'
 
 export function Dialog() {
-  const dialog = document.querySelector("dialog");
-  const showButton = document.querySelector("dialog + button");
-  const closeButton = document.querySelector("dialog button");
+  // não deveríamos fazer buscas no DOM desse jeito!
+  //const dialog = document.querySelector("dialog");
+ 
 
   // "Show the dialog" button opens the dialog modally
-  showButton.addEventListener("click", () => {
+ const openDialog = () => {
     dialog.showModal();
-  });
+  };
 
   // "Close" button closes the dialog
-  closeButton.addEventListener("click", () => {
+ const closeDialog = () => {
     dialog.close();
-  });
+  };
 
   return (
     <React.Fragment>
       <dialog>
-        <button autofocus>Close</button>
+        <button autoFocus onClick={closeDialog}>Close</button>
         <p>This modal dialog has a groovy backdrop!</p>
       </dialog>
-      <button>Show the dialog</button>
+      <button onClick={openDialog}>Show the dialog</button>
     </React.Fragment>
   );
 }

febd1c94e560a482f9d840bfa7ff84b384a796a6
robsonsoaresid <robemsuave@gmail.com>
2026-08-03 14:58:09 -0300

    Conhecendo o useRef

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './dialog.style.css'
 
 export function Dialog() {
   // não deveríamos fazer buscas no DOM desse jeito!
   //const dialog = document.querySelector("dialog");
- 
 
+const dialogRef = useRef(null)
+ 
   // "Show the dialog" button opens the dialog modally
  const openDialog = () => {
-    dialog.showModal();
+    dialogRef.current.showModal();
   };
 
   // "Close" button closes the dialog
  const closeDialog = () => {
-    dialog.close();
+    dialogRef.current.close();
   };
 
   return (
     <React.Fragment>
-      <dialog>
+      <dialog ref={dialogRef}>
         <button autoFocus onClick={closeDialog}>Close</button>
         <p>This modal dialog has a groovy backdrop!</p>
       </dialog>
